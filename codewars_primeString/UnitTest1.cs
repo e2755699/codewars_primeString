2a362119728a1c4f6c01f91880f4ec8f809c4a21
e2755699 <e2755699@gmail.com>
2017-07-27 00:31:44 +0800

    重要的testCase, Input_abaa_shoudBeReturnIsTure pass,but Input_abcaddabcadd_shouldBeReturnIsFasle not pass

--- codewars_primeString/UnitTest1.cs
+++ codewars_primeString/UnitTest1.cs
@@ -7,9 +7,60 @@ namespace codewars_primeString
     public class UnitTest1
     {
         [TestMethod]
-        public void InputAShouldBeReturnTure()
+        public void InputIsEmpty_ShouldBeReturnIsFalse()
         {
+            var expected = false;
+            var input = "";
+            AccumShouldBe(expected, input);
+        }
 
+        private static void AccumShouldBe(bool expected, string input)
+        {
+            var actual = Actual(input);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        private static bool Actual(string input)
+        {
+            var sut = new PrimeString();
+            bool actual = sut.Accum(input);
+            return actual;
+        }
+
+        [TestMethod]
+        public void Input_a_shouldReturnIsTure()
+        {
+            var input = "a";
+            var expected = true;
+            AccumShouldBe(expected, input);
+        }
+
+        [TestMethod]
+        public void Input_aa_shouldBeReturnIsFalse()
+        {
+            var input = "aa";
+            var expected = false;
+
+            AccumShouldBe(expected, input);
+        }
+
+        [TestMethod]
+        public void Input_abaa_shoudBeReturnIsTure()
+        {
+            var input = "abaa";
+            var expected = true;
+
+            AccumShouldBe(expected, input);
+        }
+
+        [TestMethod]
+        public void Input_abcaddabcadd_shouldBeReturnIsFasle()
+        {
+            var input = "abcaddabcadd";
+            var expected = false;
+
+            AccumShouldBe(expected, input);
         }
     }
 }
