c1a7d2a1b28f2c13388b3495a86e647f3167f40a
Dustin <e275566@gmail.com>
2017-08-19 18:59:07 +0800

    Refactor: extract new method "CheckString"

--- codewars_primeString/PrimeString.cs
+++ codewars_primeString/PrimeString.cs
@@ -10,24 +10,27 @@ namespace codewars_primeString
         internal bool Accum(string input)
         {
             if (string.IsNullOrEmpty(input)) return false;
-                
+
+            return CheckStringPrime(input);
+        }
+
+        private bool CheckStringPrime(string input)
+        {
             var checkString = string.Empty;
 
-            for (var i = 0; i <= input.Length/2; i++)
+            for (var i = 0; i <= input.Length / 2; i++)
             {
-
                 checkString += input[i];
 
-                if (input.Length % (i+1) == 0 && input.Length > 1)
+                if (input.Length % (i + 1) == 0 && input.Length > 1)
                 {
                     if (!RecursieCheckPrimeString(input, checkString))
                     {
                         return false;
-                    }                    
+                    }
                 }
             }
             return true;
-
         }
 
         private bool RecursieCheckPrimeString(string input, string checkString)
